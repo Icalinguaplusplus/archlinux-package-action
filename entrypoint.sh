6f2660ff86b0f1b6070a5a58b7e5ad6cbe754d89
Clansty <i@gao4.pw>
2021-08-02 11:27:36 +0800

    use envs

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,7 +32,7 @@ fi
 # Run makepkg
 if [[ -n "$INPUT_FLAGS" ]]; then
     echo '::group::Running makepkg with flags'
-    $INPUT_ENVS sudo -u builder makepkg $INPUT_FLAGS
+    CARCH=$INPUT_CARCH sudo -u builder makepkg $INPUT_FLAGS
     echo '::endgroup::'
 fi
 
